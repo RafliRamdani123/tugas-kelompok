101a4f5255e0eaa93563a909efd8c30eb2f96871
muhamadrafi17 <168408717+muhamadrafi17@users.noreply.github.com>
2024-05-06 13:52:20 +0700

    Update and rename tugas kelompok rafi.java to tugas kelompok rafi modul 4.java

---
+++ tugas kelompok rafi modul 4.java
@@ -0,0 +1,11 @@
+package TugasNo2;
+
+public class Loopwhile {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 15) {
+            System.out.println(i);
+            i += 2;
+        }
+    }
+}
